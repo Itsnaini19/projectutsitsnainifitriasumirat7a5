04b0bcbd15fe430af8824c6686cc07b34a0e7d70
Itsnaini19 <114975254+Itsnaini19@users.noreply.github.com>
2022-11-01 14:07:02 +0700

    Add files via upload

---
+++ tugas_pert6/kedua_ketiga.py
@@ -0,0 +1,18 @@
+#nomor 2
+print ("nomor 2");
+i = 0;
+x = int(input("Masukan Bilangan :"))
+
+while i < 19:
+    i = i+3;
+    print (i);
+
+#nomor 3
+print ("nomor 3");
+i = 19;
+x = int(input("Masukan Bilangan :"))
+
+while i > 3:
+    i = i-2;
+   
+    print (i);
